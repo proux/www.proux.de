c4f354e0e7b6b7907d1c614a91a9c616ef9094dc
Nico Lindemann <n.lindemann@globit.com>
2019-05-01 16:21:32 +0200

    clean up

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="de">
@@ -26,5 +26,3 @@ class MyDocument extends Document {
     )
   }
 }
-
-export default MyDocument
