ea74fe89168b1df42e32ee13bf493fda112eb606
Nico Lindemann <n.lindemann@globit.com>
2019-05-01 15:20:08 +0200

    add meta and more

--- pages/index.js
+++ pages/index.js
@@ -15,16 +15,7 @@ import '../static/fonts/fonts.css'
 export default () => {
     return <div>
         <Head>
-            <title>proux.net</title>
-            <link rel="apple-touch-icon" sizes="180x180" href="../static/assets/apple-touch-icon.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="../static/assets/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="../static/assets/favicon-16x16.png"/>
-            <link rel="manifest" href="../static/assets/site.webmanifest"/>
-            <link rel="mask-icon" href="../static/assets/safari-pinned-tab.svg" color="#366fa5"/>
-            <link rel="shortcut icon" href="../static/assets/favicon.ico"/>
-            <meta name="msapplication-TileColor" content="#2b5797"/>
-            <meta name="msapplication-config" content="../static/assets/browserconfig.xml"/>
-            <meta name="theme-color" content="#ffffff"></meta>
+          <title>proux</title>
         </Head>
         <nav className="navbar navbar-inverse navbar-fixed-top">
             <div className="container">
